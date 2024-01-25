d399003d77c94f2419c2f897c769e2f6767d8a72
Agustin J. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2024-01-25 17:02:13 -0300

    GlobalUsings.cs created for better readability of the code

---
+++ src/API/API/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿// Global using directives
+
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using System.Text;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.IdentityModel.Tokens;
