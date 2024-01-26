3d909e2a0cdb8bbe67365cf8c76fb80623120191
Agustin J. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2024-01-26 18:07:55 -0300

    Endpoints.cs created and refactor for Program.cs

--- src/Angular/Angular/Endpoints/Endpoints.cs
+++ src/Angular/Angular/Endpoints/Endpoints.cs
@@ -1,6 +1,71 @@
-﻿namespace Angular.Endpoints;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace Angular.Endpoints;
 
 public static class Endpoints
 {
-    
+    public const string LocalLogin = "/local-login";
+    public const string LocalLogout = "/local-logout";
+
+
+    public static void MapEndpoints(this IEndpointRouteBuilder routes, IConfiguration config)
+    {
+        // Permite a los usuarios iniciar sesión enviando una solicitud POST con las credenciales de inicio de sesión.
+        // El BFF envía las credenciales al backend para obtener un token de acceso (JWT).
+        // Si la respuesta es exitosa, se crea una identidad de usuario con el token de acceso y
+        // se inicia sesión utilizando el esquema de autenticación de cookies.
+        // Además, se extraen algunos datos del JWT y se devuelven en la respuesta.
+        routes.MapPost("/local-login", async (
+            LoginRequest request,
+            HttpContext httpContext,
+            IHttpClientFactory httpClientFactory) =>
+        {
+            var client = httpClientFactory.CreateClient();
+            var baseAddress = config["ApiHost:Url"];
+            var response = await client.PostAsJsonAsync($"{baseAddress}/api/token", request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var loginResponse = await response.Content.ReadFromJsonAsync<LoginResponse>();
+
+                var claims = new List<Claim>
+                {
+                    new Claim("Access_Token", loginResponse!.Token)
+                };
+
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+                await httpContext.SignInAsync(claimsPrincipal);
+
+                // Leer el token y obtener los claims utilizando JWT
+                var handler = new JwtSecurityTokenHandler();
+                var token = handler.ReadJwtToken(loginResponse.Token);
+
+                return Results.Ok(new
+                {
+                    token.ValidTo,
+                    Name = token.Claims.Where(q => q.Type == "unique_name").FirstOrDefault()?.Value,
+                    Roles = token.Claims.Where(q => q.Type == "role").Select(q => q.Value)
+                });
+            }
+
+            return Results.Forbid();
+        });
+
+        // Permite a los usuarios cerrar sesión y se encarga de cerrar la sesión actual del usuario,
+        // lo que ocasiona borrar las cookies de autenticación.
+        routes.MapPost("/local-logout", async (HttpContext httpContext) =>
+        {
+            await httpContext.SignOutAsync();
+            return Results.Ok();
+        });
+    }
 }
+
+public record LoginRequest(string UserName, string Password);
+
+public record LoginResponse(string Token);
--- src/Angular/Angular/Program.cs
+++ src/Angular/Angular/Program.cs
@@ -1,12 +1,16 @@
+using Angular;
+using Angular.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllersWithViews();
+builder.Services.AddBffProxy();
+builder.Services.AddLocalAuthentication();
+builder.Services.AddHttpClient();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -15,6 +19,13 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapReverseProxy();
+app.MapEndpoints();
+app.MapFallbackToFile();
 app.UseRouting();
 
 
